74e9dd691b0c8463b11f5082823abaa0f97d35d9
pushp1997 <pushptyagi1@gmail.com>
2023-04-25 19:15:00 +0100

    Added ability to add task to the task list

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,23 @@ pragma solidity >=0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("My 1st Task");
+        createTask("My 2nd Task");
+        createTask("My 3rd Task");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
