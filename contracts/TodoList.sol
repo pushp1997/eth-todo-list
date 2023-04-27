2910c33af437122f35a775963c30564ea32db1ff
pushp1997 <pushptyagi1@gmail.com>
2023-04-27 16:32:20 +0100

    Added ability to mark tasks a completed adn trigger events on completion

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,14 +11,24 @@ contract TodoList {
 
     mapping(uint => Task) public tasks;
 
+    event TaskCreated(uint id, string content, bool completed);
+
+    event TaskCompleted(uint id, bool completed);
+
     constructor() public {
-        createTask("My 1st Task");
-        createTask("My 2nd Task");
-        createTask("My 3rd Task");
+        createTask("This is a demo task created.");
     }
 
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
     }
 }
